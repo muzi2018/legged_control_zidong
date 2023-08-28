a6637b01da7ae35cac03470bda0114366f94d1ad
wangweijie <3127870356@qq.com>
2023-08-28 15:01:39 +0800

    the ppt of diagram

--- src/legged_controllers/src/LeggedController.cpp
+++ src/legged_controllers/src/LeggedController.cpp
@@ -24,6 +24,7 @@
 #include <legged_wbc/WeightedWbc.h>
 #include <pluginlib/class_list_macros.hpp>
 
+
 namespace legged {
 bool LeggedController::init(hardware_interface::RobotHW* robot_hw, ros::NodeHandle& controller_nh) {
   // Initialize OCS2
